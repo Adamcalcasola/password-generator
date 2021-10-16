f393429ddae0c9d64a4dacadd93af64c4eab9149
Adam Calcasola <adam.calcasola@gmail.com>
2021-10-16 19:36:52 -0400

    fix when letters are entered in password length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -3,20 +3,29 @@ var generateBtn = document.querySelector("#generate");
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var integer = ['0','1','2','3','4','5','6','7','8','9'];
-var special = [' ','!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
+var special = [' ','!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','\\','^','_','`','{','|','}','~'];
 
 function generatePassword() {
   var pwArray = [];
   // Section to define length of password within a limit of 8 to 128
-
   var pwLength = prompt("How many characters will your password have?");
-  if (pwLength > 7 && pwLength < 129) {
-    // Section to define the complexity of the password
+
+  // This section accounts for user not entering a valid entry
+  if (pwLength < 8 || pwLength > 128 || pwLength > Math.trunc(pwLength) || isNaN(pwLength)) {
+    window.alert("Please enter an integer between 7 and 129. Try again.");
+    return "Invalid entry. Please try again.";
+  } else if (pwLength > 7 && pwLength < 129) {
+    // Section to define the types of characters the user will choose for the password
     var pwLowerCase = window.confirm("Would you like your password to inclue lower case letters?");
     var pwUpperCase = window.confirm("Would you like your password to include upper case letters?");
     var pwNumber = window.confirm("Would you like your password to include numbers?");
     var pwSpecial = window.confirm("Would you like your password to include special characters?");
     
+    if (!pwLowerCase && !pwUpperCase && !pwNumber && !pwSpecial) {
+      window.alert("You must select at least one of the four character types for your password. Try again.");
+      return "Invalid entry. Please try again.";
+    };
+
     for (let i = 0; i < pwLength; i++) {
       let charType = [];
       
@@ -37,18 +46,6 @@ function generatePassword() {
     };
     return pwArray.join(""); 
   };
-  if (pwLength < 8) {
-    window.alert("Your password must have at least 8 characters. Try again.");
-    // Possilby make a variable for invalid entry to reuse.
-    return "Invalid entry. Please try again.";
-  } else if (pwLength > 128) {
-    window.alert("Your password must have less than 129 characters. Try again");
-    return "Invalid entry. Please try again.";
-  // NEED to fix it so if you don't put an integer you will get an error message
-  } else {
-    window.alert("Please enter an integer between 7 and 129. Try again.");
-    return "Invalid entry. Please try again.";
-  };
 };
 
 // Write password to the #password input
@@ -57,7 +54,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
